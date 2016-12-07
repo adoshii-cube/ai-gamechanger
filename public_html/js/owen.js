0a3a3c3a214fd32a81ea4c31f26eb87c369c82e2
adoshi <adoshi@192.168.1.3>
2016-12-07 16:13:18 +0530

    Pending 1. Landing page image 2. Contact Us form DB/email PHP
    Single header/drawer.html created for index.html

--- public_html/js/owen.js
+++ public_html/js/owen.js
@@ -1,17 +1,30 @@
 $(document).ready(function () {
     var nav = $('a');
     if (nav.length) {
-        $('a').on('click', function (event) {
+        // Add smooth scrolling to all links
+        $("a").on('click', function (event) {
+            // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
+                // Prevent default anchor click behavior
                 event.preventDefault();
+                // Store hash
                 var hash = this.hash;
-
-                $('.mdl-layout').animate({
-                    scrollTop: $(hash).offset().top
-                }, 1800, function () {
-                    window.location.hash = hash;
-                });
-            }
+                if ($(hash).length) {
+                    // Using jQuery's animate() method to add smooth page scroll
+                    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                    $('.mdl-layout__content').animate({
+                        scrollTop: $(hash).get(0).offsetTop
+//                    scrollTop: $(hash).offset().top
+                    }, 800, function () {
+                        // Add hash (#) to URL when done scrolling (default click behavior)
+//                        window.location.hash = hash;
+                    });
+                }
+                else{
+//                    alert("Oops");
+                    window.location.href = $(this).attr('href');
+                }
+            } // End if
         });
     }
 
