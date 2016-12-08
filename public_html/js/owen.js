f0c55f8b5c6caac742a1272481c2de28550501a1
adoshi <adoshi@192.168.1.3>
2016-12-08 17:16:18 +0530

    Pending 1. Landing page image 2. Contact Us form DB/email PHP
    Single header/drawer/footer/dialog/toast.html created for all pages (not resources)

--- public_html/js/owen.js
+++ public_html/js/owen.js
@@ -19,8 +19,7 @@ $(document).ready(function () {
                         // Add hash (#) to URL when done scrolling (default click behavior)
 //                        window.location.hash = hash;
                     });
-                }
-                else{
+                } else {
 //                    alert("Oops");
                     window.location.href = $(this).attr('href');
                 }
@@ -28,34 +27,33 @@ $(document).ready(function () {
         });
     }
 
-    var inputFieldCheck = $("input").filter(function () {
-        return $.trim($(this).val()).length === 0;
-    }).length === 0;
+//    var inputFieldCheck = $("input").filter(function () {
+//        return $.trim($(this).val()).length === 0;
+//    }).length === 0;
+//
+//    if (!inputFieldCheck) {
+//        $('#show-dialog').prop('disabled', true);
+//    } else {
+//        $('#show-dialog').prop('disabled', false);
+//        $('#show-dialog').addClass('mdl-color--green-600');
+//        $('#show-dialog').addClass('mdl-color-text--white');
+//    }
 
-    if (!inputFieldCheck) {
-        $('#show-dialog').prop('disabled', true);
-    } else {
-        $('#show-dialog').prop('disabled', false);
-        $('#show-dialog').addClass('mdl-color--green-600');
-        $('#show-dialog').addClass('mdl-color-text--white');
-    }
-//    (function(){
-    $('input').keyup(function () {
-        var empty = false;
-        $('input').each(function () {
-            if ($(this).val() === '') {
-                empty = true;
-            }
-        });
-        if (empty) {
-            $('#show-dialog').prop('disabled', true);
-            $('#show-dialog').removeClass('mdl-color--green-600');
-            $('#show-dialog').removeClass('mdl-color-text--white');
-        } else {
-            $('#show-dialog').prop('disabled', false);
-            $('#show-dialog').addClass('mdl-color--green-600');
-            $('#show-dialog').addClass('mdl-color-text--white');
-        }
-    });
+//    $('input').keyup(function () {
+//        var empty = false;
+//        $('input').each(function () {
+//            if ($(this).val() === '') {
+//                empty = true;
+//            }
+//        });
+//        if (empty) {
+//            $('#show-dialog').prop('disabled', true);
+//            $('#show-dialog').removeClass('mdl-color--green-600');
+//            $('#show-dialog').removeClass('mdl-color-text--white');
+//        } else {
+//            $('#show-dialog').prop('disabled', false);
+//            $('#show-dialog').addClass('mdl-color--green-600');
+//            $('#show-dialog').addClass('mdl-color-text--white');
+//        }
 //    });
 });
